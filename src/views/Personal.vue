3b125516417cc45f6d9a7ea9d80e2af49ef1b9a3
Zero <silverfantacy@gmail.com>
2023-11-20 01:21:23 +0800

    修正登入樣式

--- src/views/Personal.vue
+++ src/views/Personal.vue
@@ -37,10 +37,13 @@ watch(openai_key, (val) => {
             <form>
 
                 <div class="mb-6">
-                    <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">OpenAI key <span class="text-red-500 text-sm">只會被儲存在本機</span></label>
+                    <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">OpenAI
+                        key <span class="text-red-500 text-sm">只會被儲存在本機</span></label>
                     <input type="text" placeholder="sk-xxx" v-model="openai_key"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                    <p class="mt-2 text-sm text-gray-500 dark:text-gray-300">如果沒有key請至<a class="text-blue-600 hover:underline dark:text-blue-500" href="https://platform.openai.com/account/api-keys" target="_blank">這裡</a>申請</p>
+                    <p class="mt-2 text-sm text-gray-500 dark:text-gray-300">如果沒有key請至<a
+                            class="text-blue-600 hover:underline dark:text-blue-500"
+                            href="https://platform.openai.com/account/api-keys" target="_blank">這裡</a>申請</p>
                 </div>
 
                 <div class="relative z-0 w-full mb-6 group">
@@ -52,15 +55,20 @@ watch(openai_key, (val) => {
                 </div>
             </form>
             <button @click="onClickSignOut()"
-                class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition mt-4">登出</button>
+                class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center  mb-2 transition mt-4">登出</button>
         </template>
 
         <template v-else>
-            <div class="absolute top-0 left-0 w-full h-full flex flex-col items-center justify-center p-5">
-                <h3 class="text-3xl font-bold dark:text-white text-center">還沒登入喔！</h3>
+            <div class="fixed top-0 left-0 w-full h-full flex flex-col items-center justify-center p-5">
+                <div class="max-w-lg mx-auto">
+                    <h3 class="text-3xl font-bold dark:text-white text-center">登入後即可使用<br>SpeakWrite</h3>
 
-                <button @click="onClickSignIn()"
-                    class="w-full text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 transition mt-4">前往登入</button>
+                    <button @click="onClickSignIn()"
+                        class="w-full text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2 transition mt-4">
+                        前往登入
+                    </button>
+                </div>
+
             </div>
 
         </template>
