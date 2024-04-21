380751fc52f4c154dad412074b45b8192f49f8cd
Zero <silverfantacy@gmail.com>
2024-04-21 11:46:37 +0800

    修改key申請連結

--- src/views/user/Personal.vue
+++ src/views/user/Personal.vue
@@ -36,13 +36,14 @@ watch(openai_key, (val) => {
             <form>
 
                 <div class="mb-6">
-                    <label for="default-input" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">OpenAI
+                    <label for="default-input"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">OpenAI
                         key <span class="text-red-500 text-sm">只會被儲存在本機</span></label>
                     <input type="text" placeholder="sk-xxx" v-model="openai_key"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                     <p class="mt-2 text-sm text-gray-500 dark:text-gray-300">如果沒有key請至<a
                             class="text-blue-600 hover:underline dark:text-blue-500"
-                            href="https://platform.openai.com/account/api-keys" target="_blank">這裡</a>申請</p>
+                            href="https://platform.openai.com/api-keys" target="_blank">這裡</a>申請</p>
                 </div>
 
                 <div class="relative z-0 w-full mb-6 group">
